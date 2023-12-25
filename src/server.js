78eb52e1ca71af68254db2d46e2499782c890765
LVQT <levietquocthinh@gmail.com>
2023-12-25 22:49:48 +0700

    install docker connect to sql by dbeaver and print db to console

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');  //common js 
 const configViewEnngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
+const mysql = require('mysql2')
 
 //import express 
 const app = express();
@@ -13,6 +14,21 @@ const hostname = process.env.hostname;
 configViewEnngine(app);
 // khai báo routes
 app.use('/test', webRoutes);
+//test  connection 
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307, // default la 3306 
+    user: 'root',
+    password: '123456',
+    database: 'hoidanit'
+});
+
+
+connection.query('SELECT * FROM Users u ;',
+    function (error, results, fields) {
+        console.log('result ', results);
+        console.log('field ', fields);
+    });
 
 
 app.listen(port, hostname, () => {
