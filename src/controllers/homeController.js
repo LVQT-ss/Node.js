6c7dffed1c4caa644cac116d24b57a2db51aa4b5
LVQT <levietquocthinh@gmail.com>
2023-12-26 16:40:33 +0700

    create user by node express

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -11,8 +11,33 @@ const getFullname = (req, res) => {
     res.send('<h1>thinh le viet quoc</h1>');
 };
 const postCreateUser = (req, res) => {
-    console.log('>> request body', req.body)
-    res.send('tao user')
+
+
+
+    let email = req.body.Email;
+    let name = req.body.myName;
+    let city = req.body.mycity;
+
+
+
+    console.log('>> request body ', email, name, city)
+
+    connection.query(
+        `INSERT INTO
+         Users(email, name, city)
+           VALUES( ?, ?, ?)`,
+        [email, name, city],
+        function (err, results) {
+            console.log(results);
+            res.send('create user success')
+        }
+    );
+
+    // cách viết ngắn hơn
+    // let {email,name,city} = req.body;
+    // INSERT INTO Users(email, name, city)
+    //     VALUES("TEST", "THINH LE", "BINH DUONG")
+
 }
 
 module.exports = {
