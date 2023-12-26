97a2d9a751b67e8a7de6e2c514402ad46e22696c
LVQT <levietquocthinh@gmail.com>
2023-12-26 11:16:58 +0700

    connection pool

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,23 @@
 require('dotenv').config();
 const mysql = require('mysql2')
 //test  connection 
-var connection = mysql.createConnection({
+// var connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT, // default la 3306 
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
+
+var connection = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT, // default la 3306 
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 module.exports = connection
