3c2b8f204e3d50d5d04a61d2c20d84da6109c639
LVQT <levietquocthinh@gmail.com>
2023-12-31 20:35:11 +0700

    compelete CRUD

--- src/routes/web.js
+++ src/routes/web.js
@@ -5,6 +5,12 @@ let router = Express.Router();
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomePage);
     router.get('/detail/user/:id', homeController.getDetailPage)
+    // CRUD 
+    router.post('/create-new-user', homeController.createNewUser)
+    router.post('/delete-user', homeController.deleteUser);
+    router.get('/edit-user/:id', homeController.getEditPage);
+    router.post('/update-user', homeController.postUpdateUser)
+
     app.get('/about', (req, res) => {
         res.send(`I'm Eric!`);
     });
