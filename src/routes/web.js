7c25eb71bc1cad4efaaf96c535ba9810e411434c
LVQT <levietquocthinh@gmail.com>
2024-01-01 20:57:00 +0700

    upload file

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,6 +1,38 @@
 import Express from "express";
-import homeController from "../controllers/homeController"
+import homeController from "../controllers/homeController";
+import multer from 'multer';
+import path from 'path';
+var appRoot = require('app-root-path');
 let router = Express.Router();
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        console.log('check approot:', appRoot)
+        cb(null, appRoot + "/src/public/image/");
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const imageFilter = function (req, file, cb) {
+    // Accept images only
+    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+        req.fileValidationError = 'Only image files are allowed!';
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+
+let upload = multer({ storage: storage, fileFilter: imageFilter });
+
+
+
+let uploadMultipleFiles = multer({ storage: storage, fileFilter: imageFilter }).array('multiple_images', 3);
+
 
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomePage);
@@ -9,7 +41,25 @@ const initWebRoute = (app) => {
     router.post('/create-new-user', homeController.createNewUser)
     router.post('/delete-user', homeController.deleteUser);
     router.get('/edit-user/:id', homeController.getEditPage);
-    router.post('/update-user', homeController.postUpdateUser)
+    router.post('/update-user', homeController.postUpdateUser);
+
+    router.get('/upload', homeController.getUploadFilePage);
+    router.post('/upload-profile-pic', upload.single('profile_pic'), homeController.handleUploadFile)
+    router.post('/upload-multiple-images', (req, res, next) => {
+        uploadMultipleFiles(req, res, (err) => {
+            if (err instanceof multer.MulterError && err.code === "LIMIT_UNEXPECTED_FILE") {
+                // handle multer file limit error here
+                res.send('LIMIT_UNEXPECTED_FILE')
+            } else if (err) {
+                res.send(err)
+            }
+
+            else {
+                // make sure to call next() if all was well
+                next();
+            }
+        })
+    }, homeController.handleUploadMultipleFiles)
 
     app.get('/about', (req, res) => {
         res.send(`I'm Eric!`);
